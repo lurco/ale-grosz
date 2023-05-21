21bf22d2c488196b1c6f24ae47cfc9a23236b0f0
Andrzej Legutko <andrzejlegutko@gmail.com>
2023-05-21 09:36:48 +0200

    adds json-server with proxy and custom routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css';
+import {useEffect} from "react";
 
 function App() {
+
+    useEffect(() => {
+        fetch('/api/v1/products').then((response) => response.json()).then(console.log);
+    },[])
+
     return (
         <>
             <h1>Alegrosz</h1>
