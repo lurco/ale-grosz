32109da64b92c57c802232a71151795c6cc49be3
Andrzej Legutko <andrzejlegutko@gmail.com>
2023-06-11 09:34:02 +0200

    add clear Cart btn to CartTotal

--- src/components/Cart/CartTotal.tsx
+++ src/components/Cart/CartTotal.tsx
@@ -1,11 +1,18 @@
-import Typography from "@mui/material/Typography";
 import {useContext} from "react";
+
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box"
 
 import {ProductCart} from "../../types/product.ts";
 import {CartContext} from "../../context/CartContext.ts";
 
 function CartTotal() {
-    const [cartProducts] = useContext(CartContext);
+    const [cartProducts, setCartProducts] = useContext(CartContext);
+
+    function clearCart(){
+        setCartProducts([]);
+    }
 
     function calculateTotal(products: ProductCart[]): string{
 
@@ -13,14 +20,25 @@ function CartTotal() {
     }
 
     return (
-        <>
+        <Box sx={{display: "flex", justifyContent: "flex-end", gap: "10px", alignItems: "center"}}>
             <Typography variant="subtitle2">
                 {cartProducts && cartProducts.length > 0
                     ? `Total cost: $${calculateTotal(cartProducts)}`
                     : 'Your cart is empty'
                 }
             </Typography>
-        </>
+            {cartProducts && cartProducts.length > 0 && (
+                <Button
+                    variant="contained"
+                    color="error"
+                    size="small"
+                    onClick={clearCart}
+                >
+                    Clear
+                </Button>
+            )}
+
+        </Box>
     );
 }
 
